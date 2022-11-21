383d0f102e2c56f9b24a17e309b96d7bab6d705b
Zachary Blackwood <zachary@streamlit.io>
2022-11-21 16:35:45 -0500

    Add support for url

--- streamlit_card/__init__.py
+++ streamlit_card/__init__.py
@@ -10,22 +10,29 @@ if _RELEASE:  # use the build instead of development if release is true
     root_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(root_dir, "frontend/build")
 
-    _streamlit_chat = components.declare_component(COMPONENT_NAME, path=build_dir)
+    _streamlit_card = components.declare_component(COMPONENT_NAME, path=build_dir)
 else:
-    _streamlit_chat = components.declare_component(
+    _streamlit_card = components.declare_component(
         COMPONENT_NAME, url="http://localhost:3000"
     )
 
 
 def card(
-    title: str, text: str, image: Optional[str] = None, key: Optional[str] = None
-) -> None:
+    title: str,
+    text: str,
+    image: Optional[str] = None,
+    url: Optional[str] = None,
+    key: Optional[str] = None,
+) -> bool:
     """Creates a UI card like component.
 
     Args:
         title (str): The title of the card.
         text (str): The text of the card.
         image (str, optional): An optional background image. Defaults to None.
+        url (str, optional): An optional url to open when the card is clicked. Defaults to None.
         key (str, optional): An optional key for the component. Defaults to None.
     """
-    return _streamlit_chat(title=title, text=text, image=image, key=key)
+    return _streamlit_card(
+        title=title, text=text, image=image, url=url, key=key, default=False
+    )
