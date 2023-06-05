339dbbbb23bd5d54741c21eb3dba46d718f4977f
Gamliel Cohen <gamliel.cohen-ext@pernod-ricard.com>
2023-06-05 14:45:07 +0200

    package(): bumped the version and deleted unnecessary requirement

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as readme_file:
 
 setuptools.setup(
     name="streamlit-card",
-    version="0.0.4",
+    version="0.0.5",
     author="gamcoh",
     author_email="cohengamliel8@gmail.com",
     description="A streamlit component, to make UI cards",
